fda4b97e2fadf1bf4aa29c7e5af8a93493a2b179
Boros Csaba <boroscsaba00@yahoo.com>
2023-11-26 01:36:37 +0100

    bassza meg - csabi control

--- src/control_node.cpp
+++ src/control_node.cpp
@@ -17,6 +17,9 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/project_inliers.h>
 #include "turtlesim/Pose.h"
+#include <nav_msgs/Odometry.h>
+#include <tf2/utils.h>
+#include <geometry_msgs/Quaternion.h>
 
 class turtle_bot_control 
 {
@@ -24,7 +27,33 @@ class turtle_bot_control
     public:
         turtle_bot_control(): nh("~")
         {
+            cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+            pose_sub = nh.subscribe("/pose", 1, &turtle_bot_control::turtle_pose_callback, this);
+
+            goal_x = -1.5;
+            goal_y = 1.5;
+
+            loop_rate = 10;
+            ros::Rate rate(loop_rate);
             
+            while (1)
+            {
+                double angle = atan2(goal_y - current_y, goal_x - current_x);
+                nav_msgs::Odometry odometry;
+                
+                
+                double yaw = tf2::getYaw(odometry.pose.pose.orientation);
+                if(abs(yaw-angle)>0.5)
+                {
+                    control_turtle(0.05, 0.2);
+                }
+                else control_turtle(0.05, 0);
+                
+
+                rate.sleep();
+
+                ros::spinOnce();
+            }
         }
 
     private:
@@ -36,49 +65,42 @@ class turtle_bot_control
         double current_y;
 
         double goal_x;
-        double goal_y
-}
+        double goal_y;
 
+        int loop_rate;
 
+        const double dist_treshold = 0.05;
 
+        void turtle_pose_callback(const nav_msgs::Odometry::ConstPtr& odom_msg)
+        {
+            current_x = odom_msg->pose.pose.position.x;
+            current_y = odom_msg->pose.pose.position.y;
+        }
 
+        void control_turtle(double linear_vel, double angular_vel)
+        {
+            geometry_msgs::Twist cmd_vel_msg;
+            cmd_vel_msg.linear.x = linear_vel;
+            cmd_vel_msg.angular.z = angular_vel;
+            cmd_vel_pub.publish(cmd_vel_msg);
+        }
 
-
-
+        void stop_turtle()
+        {
+            control_turtle(0,0);
+        }
+        
+        double calculate_distance(double x1, double y1, double x2, double y2)
+        {
+            return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        }
+};
 
 int main(int a, char** aa) {
 
     ros::init(a, aa, "control");
 
-    ros::NodeHandle nh;
-
-    ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
-
-    ros::Rate loop_rate(10);
-
-    double steering_angle = 0.2;
-
-    while (ros::ok()) {
-        // Create a Twist message
-        geometry_msgs::Twist cmd_vel_msg;
-
-        // Set linear velocity (constant speed)
-        cmd_vel_msg.linear.x = 0.2; // Adjust the speed as needed
-
-        // Set angular velocity based on the specified steering angle
-        cmd_vel_msg.angular.z = steering_angle;
-
-        // Publish the Twist message
-        cmd_vel_pub.publish(cmd_vel_msg);
-
-        // Sleep to maintain the loop rate
-        loop_rate.sleep();
-        
-        // Spin once to trigger the callbacks
-        ros::spinOnce();
-
-        
-    }
+    turtle_bot_control control;
 
     return 0;
 }
